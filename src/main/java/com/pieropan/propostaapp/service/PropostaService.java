9372c21561f01bb72d0d30f20e78162692786f95
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2024-06-23 10:18:43 -0300

    Criando service

---
+++ src/main/java/com/pieropan/propostaapp/service/PropostaService.java
@@ -0,0 +1,13 @@
+package com.pieropan.propostaapp.service;
+
+import com.pieropan.propostaapp.dto.PropostaRequestDTO;
+import com.pieropan.propostaapp.dto.PropostaResponseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PropostaService {
+
+    public PropostaResponseDTO criar(PropostaRequestDTO requestDTO){
+        return null;
+    }
+}
