a0658af336c04b506140cca4abf6f743c7463088
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2024-06-23 20:39:35 -0300

    Otimização com CASCADE

--- src/main/java/com/pieropan/propostaapp/entity/Proposta.java
+++ src/main/java/com/pieropan/propostaapp/entity/Proposta.java
@@ -27,7 +27,7 @@ public class Proposta {
 
     private String observacao;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 }
