9b3550b558331bae63b40e58c312ff3e843b89e0
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2024-06-23 11:10:33 -0300

    Criando nossa repository - Interagindo com banco de dados

--- src/main/java/com/pieropan/propostaapp/controller/PropostaController.java
+++ src/main/java/com/pieropan/propostaapp/controller/PropostaController.java
@@ -3,12 +3,15 @@ package com.pieropan.propostaapp.controller;
 import com.pieropan.propostaapp.dto.PropostaRequestDTO;
 import com.pieropan.propostaapp.dto.PropostaResponseDTO;
 import com.pieropan.propostaapp.service.PropostaService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/proposta")
 public class PropostaController {
--- src/main/java/com/pieropan/propostaapp/service/PropostaService.java
+++ src/main/java/com/pieropan/propostaapp/service/PropostaService.java
@@ -2,12 +2,19 @@ package com.pieropan.propostaapp.service;
 
 import com.pieropan.propostaapp.dto.PropostaRequestDTO;
 import com.pieropan.propostaapp.dto.PropostaResponseDTO;
+import com.pieropan.propostaapp.entity.Proposta;
+import com.pieropan.propostaapp.repository.PropostaRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class PropostaService {
 
+    private PropostaRepository propostaRepository;
+
     public PropostaResponseDTO criar(PropostaRequestDTO requestDTO){
+        propostaRepository.save();
         return null;
     }
 }
