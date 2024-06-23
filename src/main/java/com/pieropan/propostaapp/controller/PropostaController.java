1c4795a7186d9b1869b3243b922a1e751efab54b
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2024-06-23 20:46:39 -0300

    Seguindo as boas práticas do REST

--- src/main/java/com/pieropan/propostaapp/controller/PropostaController.java
+++ src/main/java/com/pieropan/propostaapp/controller/PropostaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @AllArgsConstructor
 @RestController
@@ -21,6 +22,9 @@ public class PropostaController {
     @PostMapping
     public ResponseEntity<PropostaResponseDTO> criar(@RequestBody PropostaRequestDTO requestDTO){
         PropostaResponseDTO response = propostaService.criar(requestDTO);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.getId())
+                .toUri()).body(response);
     }
 }
