e9023923cb5b7030a4d214c1d6ac003561362906
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2024-06-24 20:12:41 -0300

    Melhorando nosso código utilizando application.properties e boas práticas

--- src/main/java/com/pieropan/propostaapp/service/PropostaService.java
+++ src/main/java/com/pieropan/propostaapp/service/PropostaService.java
@@ -6,11 +6,11 @@ import com.pieropan.propostaapp.entity.Proposta;
 import com.pieropan.propostaapp.mapper.PropostaMapper;
 import com.pieropan.propostaapp.repository.PropostaRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Service
 public class PropostaService {
 
@@ -18,12 +18,22 @@ public class PropostaService {
 
     private NotificacaoService notificacaoService;
 
+    private String exchange;
+
+    public PropostaService(PropostaRepository propostaRepository, NotificacaoService notificacaoService,
+                           @Value("${rabbitmq.propostapendente.exchange}")
+                           String exchange) {
+        this.propostaRepository = propostaRepository;
+        this.notificacaoService = notificacaoService;
+        this.exchange = exchange;
+    }
+
     public PropostaResponseDTO criar(PropostaRequestDTO requestDTO){
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDTO);
         propostaRepository.save(proposta);
 
         PropostaResponseDTO response = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
-        notificacaoService.notificar(response, "proposta-pendente.ex");
+        notificacaoService.notificar(response, exchange);
 
         return response;
     }
