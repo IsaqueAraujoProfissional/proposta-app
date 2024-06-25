365bc8a4784e3842fafa97e07847df9f4b558a8e
Isaque Araújo <isaquearaujo.profissional@gmail.com>
2024-06-24 21:15:28 -0300

    Testando fluxo completo - Resiliência implementada

--- src/main/java/com/pieropan/propostaapp/entity/Usuario.java
+++ src/main/java/com/pieropan/propostaapp/entity/Usuario.java
@@ -1,5 +1,6 @@
 package com.pieropan.propostaapp.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,5 +29,6 @@ public class Usuario {
     private Double renda;
 
     @OneToOne(mappedBy = "usuario")
+    @JsonBackReference
     private Proposta proposta;
 }
